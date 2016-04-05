8d612b525bd98a0446095370f617ed3e27b56fd1
Debora Fernandes <debborafernandess@gmail.com>
2016-04-04 18:14:33 -0700

    Add Feature: -Visitor visits Emprego Ja home page

--- spec/features/visitor/visitor_visits_home_page_spec.rb
+++ spec/features/visitor/visitor_visits_home_page_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 feature 'Visitor visits Emprego Ja home page' do
   scenario 'successfully' do
     visit root_path
-    expect(page).to have_content('Emprego Já')
+    expect(page).to have_css :h1, 'Emprego Já'
+    expect(page).to have_css :h3, 'Quem Somos'
+    expect(page).to have_css :p, 'Somos a ferramenta que faltava para você encontrar seu novo emprego!'
   end
 end
